52948aec644ae2bab6f97764fc86df1485ee7bbe
Luthfi Aji W <41244004+luthfiajiw@users.noreply.github.com>
2018-10-27 08:16:02 +0700

    Update index.js
    add reset function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let tambah = document.getElementById('tambah')
 let kurang = document.getElementById('kurang')
 let angka = document.getElementById('angka')
+//let reset = document.getElementById('reset')
 let nilai = 0
 
 tambah.addEventListener("click", function(){
@@ -10,3 +11,9 @@ tambah.addEventListener("click", function(){
 kurang.addEventListener("click", function(){
     angka.innerHTML=nilai-=1
 })
+
+// reset.addEventListener("click", function(){
+//     angka.innerHTML = nilai*0
+// })
+
+//I've added the reset function
